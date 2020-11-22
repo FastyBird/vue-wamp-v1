431f89affee6e29a12e13db80e7811a61ad5968c
Adam Kadlec <adam.kadlec@gmail.com>
2020-11-22 23:25:37 +0100

    Code cleaning

--- vue-wamp-v1.d.ts
+++ vue-wamp-v1.d.ts
@@ -1,4 +1,5 @@
 import Vue, { PluginFunction, VueConstructor } from 'vue';
+
 import { WampClientInterface, WampClientOptionsInterface, RpCallResponse, RpCallPromise } from './src/lib/types';
 
 interface InstallFunction extends PluginFunction<any> {
