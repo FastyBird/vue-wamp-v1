d449124e44e6245467b7e6535bf595496407ea71
Adam Kadlec <adam.kadlec@gmail.com>
2020-11-22 23:21:17 +0100

    Fixing path

--- vue-wamp-v1.d.ts
+++ vue-wamp-v1.d.ts
@@ -1,5 +1,5 @@
 import Vue, { PluginFunction, VueConstructor } from 'vue';
-import { WampClientInterface, WampClientOptionsInterface, RpCallResponse, RpCallPromise } from '@/lib/types';
+import { WampClientInterface, WampClientOptionsInterface, RpCallResponse, RpCallPromise } from './src/lib/types';
 
 interface InstallFunction extends PluginFunction<any> {
   installed?: boolean;
